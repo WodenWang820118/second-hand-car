d45788f191e9f913f3c86aeff0548fad55312555
Guan-Xin <alex0972112871@gmail.com>
2022-08-07 11:42:38 +0800

    authentication with firebase; envs setting; account menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { LandingPageComponent } from './components/landing-page/landing-page.com
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationPageComponent } from './components/authentication-page/authentication-page.component';
+import { AuthService } from './services/auth.service';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'auth', component: AuthenticationPageComponent},
-  {path: 'mainpage', component: MainpageComponent},
+  {path: 'mainpage', component: MainpageComponent, canActivate: [AuthService]},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
